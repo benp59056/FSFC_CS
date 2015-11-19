2296e937e0fc4dde54be0205ca83e1b6ff489da5
benp59056 <bpickering.59056@farnborough.ac.uk>
2015-11-19 11:49:15 +0000

    Added binary searching into ArrayUtils
    Added binary searching into ArrayUtils

--- AS_ComputerScience/src/net/picklez/as/exercises/arrays/sorting/BinarySearch.java
+++ AS_ComputerScience/src/net/picklez/as/exercises/arrays/sorting/BinarySearch.java
@@ -1,5 +1,7 @@
 package net.picklez.as.exercises.arrays.sorting;
 
+import net.picklez.utils.ArrayUtils;
+
 /**
  * @author Ben Pickering
  * @since 19 Nov 2015 | 11:28:04
@@ -7,6 +9,10 @@ package net.picklez.as.exercises.arrays.sorting;
 
 public class BinarySearch {
 
-	
+	public static void main(String[] args) {
+		int[] array = { 5, 12, 17, 23, 38, 44, 77, 84, 90 };
+		
+		System.out.println(ArrayUtils.binarySearch(array, 91));
+	}
 	
 }
