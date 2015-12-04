b31bd708e5e81a87f20d57279e5ccc0c579fe6b4
benp59056 <bpickering.59056@farnborough.ac.uk>
2015-12-04 14:30:13 +0000

    Last update of repo
    Last update of repo

---
+++ ContactManager/src/net/picklez/AppController.java
@@ -0,0 +1,14 @@
+package net.picklez;
+
+/**
+ * @author Ben Pickering
+ * @since 4 Dec 2015 | 14:24:16
+ */
+
+public class AppController {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
